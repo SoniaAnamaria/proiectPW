f49d1b60d2e94f3cc562edc0f9388e011b4b3770
Sonia <tuculia_sonia@yahoo.com>
2021-10-03 19:34:36 +0300

    Added some properties to car

--- src/cars/models.py
+++ src/cars/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+class Car(models.Model):
+    color = models.CharField(max_length=50, default='green')
+    tyre_type = models.CharField(max_length=10, blank=True, null=True)
