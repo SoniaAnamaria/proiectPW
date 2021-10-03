c3424e745041449f74d8c0a41c50ddf3d0fdc8fd
Sonia <tuculia_sonia@yahoo.com>
2021-10-03 20:46:24 +0300

    Show cars of a person

--- src/cars/admin.py
+++ src/cars/admin.py
@@ -4,7 +4,22 @@ from django.contrib import admin
 from cars.models import Car, Person
 
 class CarAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('__str__',  'tyre_type', 'contact_email', 'owner')
+    list_editable = ('contact_email', 'owner')
+    list_filter = ('owner',)
+    search_fields = ('color', 'owner__last_name')
+    fieldsets = [
+        ['Informatii generale', {'fields': ('contact_email', 'owner')}],
+        ['Detalii', {'fields': ('tyre_type', 'color')}]
+    ]
 
-admin.site.register(Car)
-admin.site.register(Person)
+class CarInline(admin.TabularInline):
+    model = Car
+    extra = 0
+    fields = ('color', 'contact_email', 'tyre_type')
+
+class PersonAdmin(admin.ModelAdmin):
+    inlines = [CarInline]
+
+admin.site.register(Car,CarAdmin)
+admin.site.register(Person,PersonAdmin)
